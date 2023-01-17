7ca1d5c9d84dd5cfd2df04b43bf2631ac9537439
Даниил Ряйккенен <dramshtadiy1337@gmail.com>
2023-01-17 23:39:12 +0300

    trylyaya

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
-# Страница со списком мороженого
+CONS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related()[:CONS]
     # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
@@ -23,9 +23,8 @@ def group_posts(request, slug):
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления
     # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.select_related()[:CONS]
     context = {
         'group': group,
-        'posts': posts,
-    }
+        'posts': posts}
     return render(request, 'posts/group_list.html', context)
