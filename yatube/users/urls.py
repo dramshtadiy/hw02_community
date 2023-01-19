2a31bd976ba0a4b26281a99b273d7d045ca0c248
Даниил Ряйккенен <dramshtadiy1337@gmail.com>
2023-01-20 00:52:06 +0300

    commitjustcommit

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,13 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeDoneView,
+    PasswordChangeView,
+    PasswordResetCompleteView,
+    PasswordResetConfirmView,
+    PasswordResetDoneView,
+    PasswordResetView,
+)
 from django.urls import path
 
 
@@ -21,26 +29,32 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'),
         name='password_change'),
     path(
         'password_change/done/',
-        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'),
         name='password_change_done'),
     path(
         'password_reset/',
-        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'),
         name='password_reset'),
     path(
         'password_reset/done/',
-        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'),
         name='password_reset_done'),
     path(
         'reset/<uidb64>/<token>/',
-        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confirm.html'),
         name='password_reset_confirm'),
     path(
         'reset/done',
-        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'),
         name='password_reset_complete'),
 ]
